c1b1a4bf89ef39b92c3592dd5147fca24deede45
Rogger Matamoros <roggmatz@gmail.com>
2017-03-13 17:35:06 -0400

    Added content blurring on menu activation.

--- page-header/script.js
+++ page-header/script.js
@@ -3,15 +3,21 @@ document.addEventListener("DOMContentLoaded", function() {
     var mobileMenuPanel = document.getElementById('mobilePanel');
     var mobileMenu = document.querySelector('.mobile-menu-items');
     
+    //temporary stub
+    var content = document.querySelector('.content');
+    
     menuButton.addEventListener('click', function(){
         if(mobileMenuPanel.classList.contains('reveal')) {
             mobileMenuPanel.classList.remove('reveal');
             mobileMenu.classList.remove('reveal');
             mobileMenuPanel.classList.add('hide');
             mobileMenu.classList.add('hide');
+            content.classList.remove('blur');
+
         } else {
             mobileMenuPanel.classList.add('reveal');
             mobileMenu.classList.add('slidedown');
+            content.classList.add('blur');
         } 
     });
     mobileMenuPanel.addEventListener('animationend', function(event) {
