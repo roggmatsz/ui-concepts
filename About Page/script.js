bde066a87c2be01b3a1af0ed8a5334727f49eb39
Rogger Matamoros <roggmatz@gmail.com>
2017-05-16 00:09:03 -0400

    Checkpoint.

---
+++ About Page/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.clickable')
+     .addEventListener('click', function() {
+        //toggle ul display
+     });
+});
